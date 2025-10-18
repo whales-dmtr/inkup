67c8c218321e43b1a6888691a7d097f0e9d4dcfa
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-10-18 22:55:59 +0100

    refactor: simplify code without changing functionality

--- backend/inkup/posts/urls.py
+++ backend/inkup/posts/urls.py
@@ -4,8 +4,6 @@ from posts import views
 
 
 urlpatterns = [
-    path('<int:pk>', views.PostDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.PostUpdateAPIView.as_view()),
-    path('<int:pk>/delete/', views.PostDeleteAPIView.as_view()),
     path('', views.PostListCreateAPIView.as_view()),
+    path('<int:pk>/', views.PostRetrieveUpdateDestroyAPIView.as_view())
 ]
--- backend/inkup/posts/views.py
+++ backend/inkup/posts/views.py
@@ -1,14 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from posts.models import Posts
 from posts.serializers import PostsSerializer
 
 
-class PostDetailAPIView(generics.RetrieveAPIView):
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
 
     # lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        print("The instance has updated.")
+        instance = serializer.save()
+
+    def perform_destroy(self, instance):
+        print("The instance has deleted.")
+        super().perform_destroy(instance)
 
 
 class PostListCreateAPIView(generics.ListCreateAPIView):
@@ -19,20 +27,3 @@ class PostListCreateAPIView(generics.ListCreateAPIView):
         print(serializer.validated_data.get('content'))
         serializer.save()
 
-
-class PostUpdateAPIView(generics.UpdateAPIView):
-    queryset = Posts.objects.all()
-    serializer_class = PostsSerializer
-
-    def perform_update(self, serializer):
-        print("The instance has updated.")
-        instance = serializer.save()
-
-
-class PostDeleteAPIView(generics.DestroyAPIView):
-    queryset = Posts.objects.all()
-    serializer_class = PostsSerializer
-
-    def perform_destroy(self, instance):
-        print("The instance has deleted.")
-        super().perform_destroy(instance)
