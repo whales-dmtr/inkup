6f0e9c6f4b574840447f9ad7b460933411f82de7
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-11-05 20:27:03 +0000

    fix: post editing functionality

--- backend/inkup/posts/views.py
+++ backend/inkup/posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
-from rest_framework.views import APIView, Response
+from rest_framework.views import Response
+from rest_framework.exceptions import APIException
 
 from posts.models import Post
 from posts.serializers import PostsSerializer
@@ -23,3 +24,18 @@ class PostListCreateAPIView(generics.ListCreateAPIView):
 class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostsSerializer
+
+    def partial_update(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        data = self.serializer_class(post).data
+        
+        for k in request.data.keys():
+            if k in ["id", "time_created", "author"]:
+                raise APIException(f"The \"{k}\" field cannot be edited.")
+        data.update(request.data)
+
+        serializer = self.serializer_class(post, data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
