9d3d483ad5d650db31fe8e8be9db75bf1315d181
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-11-05 17:46:08 +0000

    fix: posts now have an author

--- backend/inkup/posts/views.py
+++ backend/inkup/posts/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics
-from django.http import QueryDict
+from rest_framework import generics, status
+from rest_framework.views import APIView, Response
 
 from posts.models import Post
 from posts.serializers import PostsSerializer
@@ -8,6 +8,16 @@ from posts.serializers import PostsSerializer
 class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostsSerializer
+
+    def create(self, request):
+        data = request.data
+        data.update({"author": request.user.pk})
+
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+            
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
 class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
